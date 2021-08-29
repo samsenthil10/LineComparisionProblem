0cb7666f01a663a1a3a1fe48fe1adc2c99a13db6
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 05:10:47 +0530

    Added: To display welcome to line comparison computation program

---
+++ src/com/bridgelabz/linecomparisonproblem/LineComparisonComputationProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class LineComparisonComputationProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------Welcome To Line Computation Program----------");
+	}
+}
