4fad0d37a7e2939a3986ac290d3ab1c5f0f33940
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 05:32:44 +0530

    Modified: To call abstract methods to compare lines

--- src/com/bridgelabz/linecomparisonproblem/LineComparisonComputationProgram.java
+++ src/com/bridgelabz/linecomparisonproblem/LineComparisonComputationProgram.java
@@ -1,39 +1,42 @@
 package com.bridgelabz.linecomparisonproblem;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LineComparisonComputationProgram {
 
 	public static void main(String[] args) {
-
-		System.out.println("----------Welcome To Line Comparison Computation Program----------");
-		int xStartingCoordinate,xEndingCoordinate,yStartingCoordinate,yEndingCoordinate;
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter Line 1 Coordinates (x1,y1) and (x2,y2): ");
-		xStartingCoordinate=scanner.nextInt();
-		yStartingCoordinate=scanner.nextInt();
-		xEndingCoordinate=scanner.nextInt();
-		yEndingCoordinate=scanner.nextInt();
-		Double lengthOfLine1 = Double.valueOf(Math.sqrt(Math.pow((xEndingCoordinate-xStartingCoordinate),2)+Math.pow((yEndingCoordinate-yStartingCoordinate),2)));
-		
-		System.out.print("Enter Line 2 Coordinates (x1,y1) and (x2,y2): ");
-		xStartingCoordinate=scanner.nextInt();
-		yStartingCoordinate=scanner.nextInt();
-		xEndingCoordinate=scanner.nextInt();
-		yEndingCoordinate=scanner.nextInt();
-		Double lengthOfLine2 = Double.valueOf(Math.sqrt(Math.pow((xEndingCoordinate-xStartingCoordinate),2)+Math.pow((yEndingCoordinate-yStartingCoordinate),2)));
-		
+		LineComparisonIF lineComparision = new LineComparisonImpl();
+		System.out.println("----------Welcome To Line Comparison Computation Program----------");
+		Point startPoint = new Point();
+		Point endPoint = new Point();
+
+		System.out.print("Enter Starting X Coordinate of Line 1: ");
+		startPoint.setxCoordinate(scanner.nextInt());
+		System.out.print("Enter Starting Y Coordinate of Line 1: ");
+		startPoint.setyCoordinate(scanner.nextInt());
+		System.out.print("Enter Ending X Coordinate of Line 1: ");
+		endPoint.setxCoordinate(scanner.nextInt());
+		System.out.print("Enter Ending Y Coordinate of Line 1: ");
+		endPoint.setyCoordinate(scanner.nextInt());
+		Double lengthOfline1= lineComparision.lengthOfLine(startPoint, endPoint);
+
+		System.out.print("Enter Starting X Coordinate of Line 2: ");
+		startPoint.setxCoordinate(scanner.nextInt());
+		System.out.print("Enter Starting Y Coordinate of Line 2: ");
+		startPoint.setyCoordinate(scanner.nextInt());
+		System.out.print("Enter Ending X Coordinate of Line 2: ");
+		endPoint.setxCoordinate(scanner.nextInt());
+		System.out.print("Enter Ending Y Coordinate of Line 2: ");
+		endPoint.setyCoordinate(scanner.nextInt());
+		Double lengthOfline2= lineComparision.lengthOfLine(startPoint, endPoint);
 		scanner.close();
-		
-		if(lengthOfLine1.equals(lengthOfLine2)) {
-			System.out.println("Two Lines Are Equal");
-		}
-		else if(lengthOfLine1.compareTo(lengthOfLine2) < 0) {
-			System.out.println("Line 1 is shorter than Line 2");
-		}
-		else if(lengthOfLine1.compareTo(lengthOfLine2) > 0) {
-			System.out.println("Line 1 is longer than Line 2");
-		}
+
+		System.out.println();
+
+		lineComparision.checkEqualLines(lengthOfline1, lengthOfline2);
+		lineComparision.compareLines(lengthOfline1, lengthOfline2);
+
 		System.out.println("------------------------------------------------------------------");
 	}
 }
