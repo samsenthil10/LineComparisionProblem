5ec754ae5b0c3aa4eb1c5c6df2d32ca0673afa1a
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 05:34:44 +0530

    Added: To store line's coordinates using point class's objects

---
+++ src/com/bridgelabz/linecomparisonproblem/Point.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class Point {
+	
+	int xCoordinate;
+	int yCoordinate;
+	
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public void setyCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+}
