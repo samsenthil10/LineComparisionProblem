adeccf799712893e95698ad6a2e44bd65a109609
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-31 11:47:58 +0530

    Modified: To retrieve coordinates data using getters

--- src/com/bridgelabz/linecomparisonproblem/Point.java
+++ src/com/bridgelabz/linecomparisonproblem/Point.java
@@ -5,6 +5,14 @@ public class Point {
 	int xCoordinate;
 	int yCoordinate;
 	
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public int getyCoordinate() {
+		return yCoordinate;
+	}
+
 	public void setxCoordinate(int xCoordinate) {
 		this.xCoordinate = xCoordinate;
 	}
