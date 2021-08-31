2e3624884fca00e4d5da0efff72c05883f6a12f0
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-31 11:46:38 +0530

    Added: To store coordinates required for line

---
+++ src/com/bridgelabz/linecomparisonproblem/Line.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class Line {
+	
+	final Point lineStartingCoordinates, lineEndingCoordinates; 
+	
+	public Line(Point lineStartingCoordinates, Point lineEndingCoordinates) {
+		
+		this.lineStartingCoordinates = lineStartingCoordinates;
+		this.lineEndingCoordinates = lineEndingCoordinates;
+	}
+}
